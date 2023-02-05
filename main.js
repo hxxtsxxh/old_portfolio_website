a0badc57042eb95df75b146f53ab8c98330883be
mhshackclub <heet1356@gmail.com>
2023-02-05 18:48:11 -0500

    updated color to torus geometry

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ window.addEventListener("resize", () => {
 // Torus
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xc18f17 });
+const material = new THREE.MeshStandardMaterial({ color: 0xd37506 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
